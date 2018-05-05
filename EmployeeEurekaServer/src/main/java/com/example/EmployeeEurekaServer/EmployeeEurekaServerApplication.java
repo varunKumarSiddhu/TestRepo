b6dea9020ba041f1aa3919badb226a314ab900b2
vvarunku <vvarunku@VVARUNKU-IN.in.oracle.com>
2018-05-05 07:44:56 +0530

    First commit for test

--- EmployeeEurekaServer/src/main/java/com/example/EmployeeEurekaServer/EmployeeEurekaServerApplication.java
+++ EmployeeEurekaServer/src/main/java/com/example/EmployeeEurekaServer/EmployeeEurekaServerApplication.java
@@ -2,13 +2,13 @@ package com.example.EmployeeEurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
 public class EmployeeEurekaServerApplication {
     public static void main(String[] args) {
+    	// My first commnt
     	SpringApplication.run(EmployeeEurekaServerApplication.class, args);
     }
 }
